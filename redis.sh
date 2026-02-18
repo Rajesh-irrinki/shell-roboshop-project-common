e9656aaa7d7c2901f08d3d55d6a7101fa2f7388a
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-18 17:01:40 +0530

    shell-roboshop-project-common updated

---
+++ redis.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+app_name=redis
+
+dnf module disable redis -y &>>$log_file
+dnf module enable redis:7 -y &>>$log_file
+validate $? "Enabling redis 7 version"
+
+dnf install redis -y &>>$log_file
+validate $? "Installing redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf 
+validate $? "Allow remote connections"
+
+service_start $app_name
+
+execution_time
