0da646ff9dd71dd7591deacc018db5a94cea2c42
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-19 04:14:08 +0530

    shell-roboshop-project-common updated

--- shipping.sh
+++ shipping.sh
@@ -29,7 +29,7 @@ if [ $? -ne 0 ]; then
     mysql -h $mysql_host -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$log_file
     validate $? " " [ $(date '+%F %T') ] " Loaded data to mysql"
 else
-    echo -e " "[ $(date '+%F %T') ]" Data already loaded to mysql ... $Y SKIPPING $N" | tee -a $log_file
+    echo -e " "[ $(date '+%F %T') ] " Data already loaded to mysql ... $Y SKIPPING $N" | tee -a $log_file
 fi
 
 service_restart $app_name
