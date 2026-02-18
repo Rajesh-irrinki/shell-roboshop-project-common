60527d8acba49206ac1212d83cb231079e01169d
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-18 16:34:22 +0530

    shell-roboshop-project-common updated

---
+++ catalogue.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+app_name=catalogue
+mongodb_host=mongodb.rajeshirrinki.online
+
+source ./common.sh
+
+nodejs_setup
+
+user_creation roboshop
+
+app_setup 
+
+cd /app 
+npm install &>>$log_file
+validate $? "Installing Dependencies"
+
+cp $script_dir/catalogue.service /etc/systemd/system/catalogue.service &>>$log_file
+validate $? "Catalogue systemd service creation"
+
+daemon_reload
+
+service_start 
+
+cp $script_dir/mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
+validate $? "Mongo.repo creation"
+
+dnf install mongodb-mongosh -y &>>$log_file
+validate $? "mongo shell installation"
+
+index=(mongosh -h $mongodb_host --quiet --eval 'db.getMongo().getDBNames().indexOf("catalogue")' )
+if [ $index -le 0 ] then;
+    mongosh --host $mongodb_host </app/db/master-data.js
+    validate $? "Master data loading"
+else
+    echo -e " "[ $(date '+%F %T') ]" "Master data already exists ... $Y SKIPPING $N" | tee -a $log_file
+fi
+
+
+
+
+
+
--- common.sh
+++ common.sh
@@ -28,6 +28,49 @@ validate() {
     fi
 }
 
+nodejs_setup() {
+    dnf module disable nodejs -y &>>$log_file
+    dnf module enable nodejs:20 -y &>>$log_file
+    validate $? "Enabled Nodejs 20 version"
+
+    dnf install nodejs -y &>>$log_file
+    validate $? "Installing nodejs"
+}
+
+user_creation() {
+    id $1 &>>$log_file
+    if [ $? -ne 0 ]; then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" $1 &>>$log_file
+        validate $? "$1 user creation"
+    else
+        echo -e " "[ $(date '+%F %T') ]" $1 user already exists ... $Y SKIPPING $N" | tee -a $log_file
+    fi
+}
+
+app_setup() {
+
+    mkdir -p /app &>>$log_file
+    validate $? "App directory creation"
+
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$log_file
+    validate $? "Downloading catalogue"
+
+    cd /app &>>$log_file
+    validate $? "Moving to app directory" 
+
+    rm -rf /app/* &>>$log_file
+    validate $? "Removing existing code in app directory"
+
+    unzip /tmp/$app_name.zip &>>$log_file
+    validate $? "Unziping catalogue code"
+
+}
+
+daemon_reload() {
+    systemctl daemon-reload &>>$log_file
+    validate $? "Daemon reload"
+}
+
 service_start() {
     systemctl enable $app_name &>>$log_file
     systemctl start $app_name &>>$log_file
