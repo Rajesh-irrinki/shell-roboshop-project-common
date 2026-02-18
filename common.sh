6ce170882e5c023dc1277fe3bde1074406608a0c
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-18 15:52:30 +0530

    shell-roboshop-project-common updated

--- common.sh
+++ common.sh
@@ -8,7 +8,8 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 script_dir=$(cd "$(dirname "$0")" && pwd)
-
+start_time=$(date +%s)
+echo "Script execution started at : $(date '+%F %T' )"
 mkdir -p $log_folder
 
 check_root() {
