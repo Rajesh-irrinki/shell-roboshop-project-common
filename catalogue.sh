fb00347cb3c895bcfb56909a57fba6459acdfdb7
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-18 16:45:57 +0530

    shell-roboshop-project-common updated

--- catalogue.sh
+++ catalogue.sh
@@ -30,7 +30,7 @@ validate $? "Mongo.repo creation"
 dnf install mongodb-mongosh -y &>>$log_file
 validate $? "mongo shell installation"
 
-index=(mongosh -h $mongodb_host --quiet --eval 'db.getMongo().getDBNames().indexOf("catalogue")' )
+index=$(mongosh -h $mongodb_host --quiet --eval 'db.getMongo().getDBNames().indexOf("catalogue")' )
 if [ $index -le 0 ]; then
     mongosh --host $mongodb_host </app/db/master-data.js
     validate $? "Master data loading"
